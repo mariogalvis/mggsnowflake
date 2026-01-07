150499316fc1a102da241cb3c22dc50286cb2ce5
mariogalvis <mariogalvisguerrero@gmail.com>
2026-01-07 08:43:04 -0500

    Update Cortex_AISQL.sql

--- Cortex_AISQL.sql
+++ Cortex_AISQL.sql
@@ -1,4 +1,9 @@
-select snowflake.cortex.complete('openai-gpt-5','Que es ABC en Colombia?');
+SELECT AI_COMPLETE('openai-gpt-5','Que es ABC en Colombia?');
+
+SELECT AI_COMPLETE( 
+'gemini-3-pro', 
+'En menos de 100 palabras, enumere lo que se necesita para construir algo similar a la inteligencia de Snowflake que pueda responder preguntas de tipo POR QUÉ o CÓMO encadenando múltiples herramientas y conjuntos de datos y utilizando solo datos comerciales internos estructurados y no estructurados con gobernanza, auditoría y telemetría adecuadas.'
+);
 
 USE WAREHOUSE VW_GENAI;
 USE DATABASE BD_EMPRESA;
